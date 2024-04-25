faccc75d92652e3ca09576398023b13cf48fa120
NoorPinger <noorpinger2002@gmail.com>
2024-04-25 13:48:28 -0500

    fix time

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <sys/time.h>
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
@@ -15,9 +16,12 @@
 #define MENU_BUTTON D2
 #define POWER_BUTTON D0
 #define MAX_DATA 4
-#define MAX_DATA_LEN 10
+#define MAX_DATA_LEN 5
 
 
+struct timeval start, now;
+long seconds;
+long seconds_offset = 0;
 LiquidCrystal_I2C lcd(0x27, 16, 2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 /*
@@ -28,12 +32,18 @@ bool HOME = true;
 bool FRESH_START = true;
 bool SHIFT_FLAG = false;
 bool POWER = false;
+bool INVALID_DATA = false;
+bool setTime = false;
 
 /*
   Array to store the data
 */
 char data[MAX_DATA][MAX_DATA_LEN];
 int data_index = 0;
+
+char str[5];
+uint16_t hrCount = 0;
+uint16_t  minCount = 0;
 
 
 // This function initializes the array with '-' so we have a way to check if data exists or not
@@ -59,7 +69,7 @@ void shiftArray()
       data[i][j] = data[i+1][j];
     }
   }
-  for(int j = 0; j < 10; j++)
+  for(int j = 0; j < MAX_DATA; j++)
   {
     data[MAX_DATA-1][j] = '-';
   }
@@ -75,17 +85,41 @@ void shiftArray()
 class MyCallbacks: public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) {
       std::string value = pCharacteristic->getValue();
-      if(data_index == MAX_DATA)
+      if(setTime)
       {
-        shiftArray();
-        data_index = MAX_DATA-1;
+        if(value.length() > 0)
+        {
+          seconds_offset = (((int((value[0]))-48)*10 + (int((value[1]))-48)) * 3600) + (((int((value[3]))-48)*10 + (int((value[4])))-48) * 60);
+          now.tv_sec = seconds_offset;
+          gettimeofday(&start, NULL);
+          // hrCount = (int((value[0]))-48)*10 + (int((value[1]))-48);
+          // minCount = (int((value[3]))-48)*10 + (int((value[4]))-48);
+          // hrCount = int((value[3]))*10 + int(('a' - value[4]));
+          Serial.println("seconds: " + String(seconds));
+          Serial.printf("hrCount: %d, minCount: %d\n", hrCount, minCount);
+          // for(int i = 0; i < value.length(); i++)
+          //   str[i] = value[i];
+        }
+        setTime = false;
       }
+      else
+      {
+        if(data_index == MAX_DATA)
+        {
+          shiftArray();
+          data_index = MAX_DATA-1;
+        }
 
 
-      if (value.length() > 0) {
-        for (int i = 0; i < value.length(); i++)
-          data[data_index][i] = value[i];
-        data_index++;
+        if (value.length() > 0 && value.length() <= MAX_DATA_LEN) {
+          for (int i = 0; i < value.length(); i++)
+            data[data_index][i] = value[i];
+          data_index++;
+        }
+        else
+        {
+          INVALID_DATA = true;
+        }
       }
     }
 };
@@ -98,6 +132,13 @@ class MyCallbacks: public BLECharacteristicCallbacks {
 void menuPressed() {
   MENU ^= true;
   HOME = !MENU;
+}
+
+
+void d4Pressed()
+{
+  setTime = true;
+  //Serial.println("D4 Pressed");
 }
 
 
@@ -143,6 +184,7 @@ void initBLE()
 }
 
 void setup() {
+  gettimeofday(&start, NULL);
   lcd.init();                      // initialize the lcd 
   lcd.backlight();
   /*
@@ -160,12 +202,14 @@ void setup() {
   */
   pinMode(MENU_BUTTON, INPUT_PULLUP);
   pinMode(POWER_BUTTON, INPUT_PULLDOWN);
+  pinMode(D4, INPUT_PULLUP);
 
   /*
     Attaching the interrupts
   */
   attachInterrupt(digitalPinToInterrupt(MENU_BUTTON), menuPressed, FALLING);
   attachInterrupt(digitalPinToInterrupt(POWER_BUTTON), powerPressed, RISING);
+  attachInterrupt(digitalPinToInterrupt(D4), d4Pressed, FALLING);
 
   Serial.begin(115200);
   initBLE();
@@ -177,15 +221,15 @@ void setup() {
 void printContents()
 {
   uint8_t i,j = 0;
-  for(int i = 0; i < 5; i++)
+  for(int i = 0; i < MAX_DATA; i++)
   {
-      Serial.printf("[%d]: ", i+1);
-      for(j = 0; j < 10; j++)
+      //Serial.printf("[%d]: ", i+1);
+      for(j = 0; j < MAX_DATA_LEN; j++)
       {
-        if(data[i][j] != '-')
-          Serial.print(data[i][j]);
+        if(data[i][j] != '-');
+          //Serial.print(data[i][j]);
       }
-      Serial.println();
+      //Serial.println();
   }
 }
 
@@ -194,31 +238,33 @@ void sleepHandler()
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Powering Off");
-  Serial.println("Powering Off");
+  //Serial.println("Powering Off");
   delay(500);
   lcd.print(".");
   delay(1000);
   lcd.print(".");
   delay(1000);
   lcd.print(".");
+  lcd.noDisplay();
   lcd.noBacklight();
   gpio_wakeup_enable(GPIO_NUM_2, GPIO_INTR_HIGH_LEVEL);
   esp_sleep_enable_gpio_wakeup();
   delay(500);
-  Serial.flush();
+  //Serial.flush();
   esp_light_sleep_start();
 }
 
 void wakeupHandler()
 {
+  lcd.display();
   lcd.clear();
   gpio_wakeup_disable(GPIO_NUM_2);
   HOME = true;
   MENU = false;
   POWER = false;
   lcd.setCursor(0, 0);
-  lcd.print("Powering On");
   lcd.backlight();
+  lcd.print("Powering On");
   delay(500);
   lcd.print(".");
   delay(1000);
@@ -237,24 +283,50 @@ void wakeupHandler()
 */
 void printArray()
 {
-  for(int i = 0; i < 5; i++)
+  for(int i = 0; i < MAX_DATA; i++)
   {
-    for(int j = 0; j < 10; j++)
+    for(int j = 0; j < MAX_DATA_LEN; j++)
     {
-      Serial.printf("[%d][%d] %c\t",i,j,data[i][j]);
+      //Serial.printf("[%d][%d] %c\t",i,j,data[i][j]);
     }
-    Serial.println();
+    //Serial.println();
   }
 
 }
 
 
+void getTime()
+{
+  hrCount = seconds / 3600;
+  if(hrCount > 12)
+  {
+    hrCount -= 12;
+  }
+  minCount = (seconds % 3600) / 60;
+  if(minCount > 59)
+  {
+    minCount = 0;
+  }
+}
 void loop() 
 {
+  gettimeofday(&now, NULL);
+  now.tv_sec += seconds_offset;
+  seconds = (now.tv_sec - start.tv_sec);
+  getTime();
+  if(seconds >= 86400)
+  {
+    start = now;
+    seconds_offset = 0;
+    hrCount = 0;
+    minCount = 0;
+  }
   if(MENU)
   {
-    printContents();
-    delay(500);
+    Serial.println("seconds: " + String(seconds));
+    Serial.printf("hrCount: %d, minCount: %d\n", hrCount, minCount);
+    // printContents();
+    // delay(500);
   }
   else if(POWER)
   {
@@ -263,35 +335,61 @@ void loop()
   }
   else if(HOME)
   {
-    // lcd.clear();
-    // lcd.print("HOME Screen");
-    Serial.println("HOME Screen");
-    if(data_index > 0)
+    lcd.setCursor(11, 1);
+    if(hrCount == 0)
+      hrCount = 12;
+    if(hrCount < 10)
+      lcd.print("0");
+    lcd.print(hrCount);
+    if(seconds % 2 == 0)
+      lcd.print(":");
+    else
+      lcd.print(" ");
+    if(minCount < 10)
+      lcd.print("0");
+    lcd.print(minCount);
+    if(INVALID_DATA)
     {
-      // lcd.clear();
+      lcd.clear();
       lcd.setCursor(0, 0);
-      lcd.print("Latest Reading:");
-      Serial.print("Recent Value: ");
-      lcd.setCursor(0, 1);
-      for(int i = 0; i < MAX_DATA_LEN; i++)
-      {
-        if(data[data_index-1][i] != '-')
-        {
-          lcd.write(data[data_index-1][i]);
-          Serial.print(data[data_index-1][i]);
-
-        }
-      }
-      lcd.print(" mg/dL");
-      Serial.println();
-
+      lcd.print("Invalid Data");
+      //Serial.println("Invalid Data");
+      INVALID_DATA = false;
+      delay(1500);
     }
     else
     {
-      lcd.setCursor(0, 0);
-      lcd.print("Waiting");
-      Serial.println("No Data");
+      // lcd.clear();
+      // lcd.print("HOME Screen");
+      //Serial.println("HOME Screen");
+      if(data_index > 0)
+      {
+        // lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Latest Reading:");
+        //Serial.print("Recent Value: ");
+        lcd.setCursor(0, 1);
+        for(int i = 0; i < MAX_DATA_LEN; i++)
+        {
+          if(data[data_index-1][i] != '-')
+          {
+            lcd.write(data[data_index-1][i]);
+            //Serial.print(data[data_index-1][i]);
+
+          }
+        }
+        lcd.print(" mg/dL  ");
+        //Serial.println();
+
+      }
+      else
+      {
+        lcd.setCursor(0, 0);
+        lcd.print("Waiting");
+        //Serial.println("No Data");
+      }
+      // delay(500);
+
     }
-    delay(500);
   }
 }
